3765121c14e0ce33d8468d3d11c725fedfd404b6
Renato-SI <renatorrbf@gmail.com>
2026-03-24 20:59:19 -0300

    delete

--- TESTAR_apos aprender o while copy.py
+++
@@ -1,8 +0,0 @@
-#estudos04:
-dia = (input("Dia de seu nascimento:"))
-mes = (input("qual o mes de seu nascimento"))
-if mes!= ("1,2,3,4,5,6,7,8,9,10,11,12"):
-    print("tente inserir um mes valido")
-ano = (input("em qual ano voce nasceu"))
-if ano>=1970:
-    print("tente inserir um  valido")
